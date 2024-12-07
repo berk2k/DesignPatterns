7b9e0b93c5e04f60783b59699395425b34a836fc
Berk Polat <berkpolat125@gmail.com>
2024-12-07 13:56:17 +0300

    Add project files.

---
+++ DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class Logger
+    {
+        private static Logger _instance;
+
+        private Logger() { }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Logger();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+
+    }
+}
