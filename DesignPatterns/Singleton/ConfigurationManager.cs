76fdacfec1dd02326fdf758c3687b4ba22cb67a2
Berk Polat <berkpolat125@gmail.com>
2025-04-25 10:20:51 +0300

    singleton updated

---
+++ DesignPatterns/Singleton/ConfigurationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class ConfigurationManager
+    {
+        public string GetConnectionString()
+        {
+            
+            return "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/DatabaseConnection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton
+{
+    public class DatabaseConnection
+    {
+        private static DatabaseConnection _instance;
+        private static readonly object _lock = new object();
+
+        private string _connectionString;
+
+        private DatabaseConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+            Console.WriteLine("Database Connection Created with Connection String: " + _connectionString);
+        }
+
+        public static DatabaseConnection GetInstance(string connectionString)
+        {
+            // Thread-safe
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new DatabaseConnection(connectionString);
+                }
+            }
+            return _instance;
+        }
+
+        
+        public void OpenConnection()
+        {
+            Console.WriteLine("Opening database connection...");
+            
+        }
+
+        
+        public void CloseConnection()
+        {
+            Console.WriteLine("Closing database connection...");
+            
+        }
+    }
+}
