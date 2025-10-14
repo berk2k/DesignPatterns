a54673e024d588078de8da2b2b1420492ee209e6
Berk Polat <berkpolat125@gmail.com>
2025-10-14 13:32:59 +0300

    modern version

---
+++ DesignPatterns/Singleton/LoggerV2ModernVersion.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public sealed class LoggerV2ModernVersion
+    {
+        private static readonly Lazy<LoggerV2ModernVersion> _instance =
+            new Lazy<LoggerV2ModernVersion>(() => new LoggerV2ModernVersion());
+              
+        private LoggerV2ModernVersion() { }
+
+        private static LoggerV2ModernVersion Instance => _instance.Value;
+    }
+}
