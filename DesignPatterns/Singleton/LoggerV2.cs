4c9f780a275963f808e4ab37d3de3b8034a6e40a
Berk Polat <berkpolat125@gmail.com>
2025-10-14 13:29:50 +0300

    thread safe version

---
+++ DesignPatterns/Singleton/LoggerV2.cs
@@ -0,0 +1,26 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public class LoggerV2
+    {
+        private static LoggerV2 _instance;
+        private static readonly object _lock = new object();
+
+        private LoggerV2() {}
+
+        public static LoggerV2 Instance
+        {
+            get
+            {
+                if( _instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                            _instance = new LoggerV2();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
