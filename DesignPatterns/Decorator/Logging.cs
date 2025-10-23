68b87109d765a776c6a75520abca1f1a2d5530fc
Berk Polat <berkpolat125@gmail.com>
2025-10-23 11:40:25 +0300

    decorator logging

---
+++ DesignPatterns/Decorator/Logging.cs
@@ -0,0 +1,50 @@
+﻿public interface IService
+{
+    void Execute();
+}
+public class BusinessService : IService
+{
+    public void Execute()
+    {
+        Console.WriteLine("Business logic executed.");
+    }
+}
+
+public abstract class ServiceDecorator : IService
+{
+    protected readonly IService _innerService;
+
+    protected ServiceDecorator(IService innerService)
+    {
+        _innerService = innerService;
+    }
+
+    public virtual void Execute()
+    {
+        _innerService.Execute();
+    }
+}
+
+public class LoggingDecorator : ServiceDecorator
+{
+    public LoggingDecorator(IService innerService) : base(innerService) { }
+
+    public override void Execute()
+    {
+        Console.WriteLine("[LOG] Starting execution...");
+        base.Execute();
+        Console.WriteLine("[LOG] Execution finished.");
+    }
+}
+
+public class AuthorizationDecorator : ServiceDecorator
+{
+    public AuthorizationDecorator(IService innerService) : base(innerService) { }
+
+    public override void Execute()
+    {
+        Console.WriteLine("[AUTH] Checking permissions...");
+        base.Execute();
+    }
+}
+
